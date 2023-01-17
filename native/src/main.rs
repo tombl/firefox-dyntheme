3d61192087157a30294a70da3df9ceb942e2b619
Tom S <git@tombl.dev>
2023-01-18 03:13:28 +0800

    switch from polling to signals

--- native/src/main.rs
+++ native/src/main.rs
@@ -1,12 +1,16 @@
 use anyhow::Result;
 use directories::ProjectDirs;
-use notify_debouncer_mini::{new_debouncer, notify::RecursiveMode};
-use std::{io::Write, time::Duration};
+use signal_hook::{
+    consts::{SIGUSR1, SIGUSR2},
+    iterator::Signals,
+};
+use std::{io::Write, path::Path};
 
-fn post_message(data: &[u8]) -> Result<()> {
+fn send_theme(path: &Path) -> Result<()> {
+    let data = std::fs::read(path).unwrap();
     let mut stdout = std::io::stdout().lock();
     stdout.write(&u32::try_from(data.len())?.to_ne_bytes())?;
-    stdout.write(data)?;
+    stdout.write(&data)?;
     stdout.flush()?;
     Ok(())
 }
@@ -14,22 +18,14 @@ fn post_message(data: &[u8]) -> Result<()> {
 fn main() -> Result<()> {
     let project_dirs = ProjectDirs::from("dev", "tombl", "firefox-dyntheme").unwrap();
     let theme_path = project_dirs.config_dir().join("theme.json");
-    eprintln!("Watching {:?}", theme_path);
+    eprintln!("Reading from {theme_path:?}");
 
-    let data = std::fs::read(&theme_path).unwrap();
-    post_message(&data).unwrap();
+    send_theme(&theme_path).unwrap();
 
-    let (tx, rx) = std::sync::mpsc::channel();
+    let mut signals = Signals::new(&[SIGUSR1, SIGUSR2])?;
 
-    let mut debouncer = new_debouncer(Duration::from_millis(50), None, tx).unwrap();
-
-    debouncer
-        .watcher()
-        .watch(&theme_path, RecursiveMode::Recursive)?;
-
-    for _ in rx {
-        let data = std::fs::read(&theme_path).unwrap();
-        post_message(&data).unwrap();
+    for _ in signals.forever() {
+        send_theme(&theme_path).unwrap();
     }
 
     Ok(())
