f3f2e95fcd9373d09455bc655209b73f7479967c
Tom S <git@tombl.dev>
2023-01-18 02:35:24 +0800

    make it work

--- ext/background.js
+++ ext/background.js
@@ -1,27 +1,13 @@
-// browser.theme.update({
-//   images: {},
-//   properties: {},
-//   colors: {
-//     toolbar: "#282c34",
-//     toolbar_text: "#abb2bf",
-//     frame: "#202329",
-//     tab_background_text: "#abb2bf",
-//     toolbar_field: "#282c34",
-//     toolbar_field_text: "#abb2bf",
-//     tab_line: "#282c34",
-//     popup: "#202329",
-//     popup_text: "#abb2bf",
-//     tab_loading: "#282c34",
-//   },
-// });
-
 try {
-  const port = browser.runtime.connectNative("dev.tombl.firefox_dyntheme");
+  const port = browser.runtime.connectNative("dev.tombl.dyntheme");
+  console.log(port.name);
 
   port.onMessage.addListener((msg) => {
     console.log("reloading theme");
-    browser.theme.update(JSON.parse(msg));
+    browser.theme.update(msg);
   });
+
+  port.postMessage("reload");
 } catch (error) {
   console.error(error.toString());
 }
--- native/src/main.rs
+++ native/src/main.rs
@@ -1,7 +1,36 @@
 use anyhow::Result;
+use directories::ProjectDirs;
+use notify_debouncer_mini::{new_debouncer, notify::RecursiveMode};
+use std::{io::Write, time::Duration};
+
+fn post_message(data: &[u8]) -> Result<()> {
+    let mut stdout = std::io::stdout().lock();
+    stdout.write(&u32::try_from(data.len())?.to_ne_bytes())?;
+    stdout.write(data)?;
+    stdout.flush()?;
+    Ok(())
+}
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    let project_dirs = ProjectDirs::from("dev", "tombl", "firefox-dyntheme").unwrap();
+    let theme_path = project_dirs.config_dir().join("theme.json");
+    eprintln!("Watching {:?}", theme_path);
+
+    let data = std::fs::read(&theme_path).unwrap();
+    post_message(&data).unwrap();
+
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    let mut debouncer = new_debouncer(Duration::from_millis(50), None, tx).unwrap();
+
+    debouncer
+        .watcher()
+        .watch(&theme_path, RecursiveMode::Recursive)?;
+
+    for _ in rx {
+        let data = std::fs::read(&theme_path).unwrap();
+        post_message(&data).unwrap();
+    }
 
     Ok(())
 }
